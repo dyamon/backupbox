1d82be58f952e9121bdb965015c6ce026eda1bfd
Federico Igne <git@federicoigne.com>
2021-05-08 00:35:10 +0100

    Implement page navigation via prev/next buttons

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,60 @@ use gio::prelude::*;
 use gio::VolumeMonitor;
 use gtk::prelude::*;
 use gtk::Application;
+use std::cmp;
+use std::{cell::RefCell, rc::Rc};
+
+struct GUIState {
+    page: u8,          // current page
+    tot: u8,           // total pages
+    body: gtk::Stack,  // main content
+    prev: gtk::Button, // "prev" button
+    next: gtk::Button, // "next" button
+}
+
+impl GUIState {
+    fn new(tot: u8, body: gtk::Stack) -> Self {
+        let mut state = GUIState {
+            page: 1,
+            tot,
+            body,
+            prev: gtk::Button::new(),
+            next: gtk::Button::new(),
+        };
+        state.update();
+        state
+    }
+
+    fn next(&mut self) {
+        self.page = cmp::min(self.page + 1, self.tot);
+        self.update();
+    }
+
+    fn prev(&mut self) {
+        self.page = cmp::max(self.page - 1, 1);
+        self.update();
+    }
+
+    fn set_prev_button(&mut self, prev: gtk::Button) {
+        self.prev = prev;
+        self.update();
+    }
+
+    fn set_next_button(&mut self, next: gtk::Button) {
+        self.next = next;
+        self.update();
+    }
+
+    fn update(&mut self) {
+        self.body.set_visible_child_name(&format!("{}", self.page));
+        self.prev.set_sensitive(self.page != 1);
+        if self.page == self.tot {
+            self.next.set_label("Backup!");
+        } else {
+            self.next.set_label("Next");
+        }
+    }
+}
 
 fn main() {
     let application = Application::new(Some("com.federicoigne.backupbox"), Default::default())
@@ -24,6 +78,45 @@ fn setup_gui(app: &Application) {
         .expect("Unable to find 'main' Window object");
     window.set_application(Some(app));
 
+    let body: gtk::Stack = builder
+        .get_object("body")
+        .expect("Unable to find 'body' Stack object");
+
+    // Setup steps
+    let welcome = gtk::Label::new(Some("Welcome to BackupBox!"));
+    body.add_named(&welcome, "1");
+    let step1 = gtk::Label::new(Some("Step 1: select origin drive"));
+    body.add_named(&step1, "2");
+    let step2 = gtk::Label::new(Some("Step 2: select target drive"));
+    body.add_named(&step2, "3");
+    body.set_visible_child_name("1");
+
+    // Initialize state
+    let state = Rc::new(RefCell::new(GUIState::new(3, body)));
+
+    // Updating button callback does not work, you should recreate the buttons
+    let prev: gtk::Button = builder
+        .get_object("prev")
+        .expect("Unable to find 'prev' Button object");
+    prev.connect_clicked({
+        let s = Rc::clone(&state);
+        move |_| s.borrow_mut().prev()
+    });
+
+    let next: gtk::Button = builder
+        .get_object("next")
+        .expect("Unable to find 'next' Button object");
+    next.connect_clicked({
+        let s = Rc::clone(&state);
+        move |_| s.borrow_mut().next()
+    });
+
+    {
+        let mut mstate = state.borrow_mut();
+        mstate.set_prev_button(prev);
+        mstate.set_next_button(next);
+    }
+
     // Setup clock in headerbar
     setup_clock(
         &builder
@@ -38,21 +131,6 @@ fn setup_gui(app: &Application) {
             .expect("Unable to find 'clock' Label object"),
     );
 
-    let body: gtk::Stack = builder
-        .get_object("body")
-        .expect("Unable to find 'body' Stack object");
-
-    // Setup steps
-    let welcome = gtk::Label::new(Some("Welcome to BackupBox!"));
-    body.add_named(&welcome, "0");
-    let step1 = gtk::Label::new(Some("Step 1: select origin drive"));
-    body.add_named(&step1, "1");
-    let step2 = gtk::Label::new(Some("Step 2: select target drive"));
-    body.add_named(&step2, "2");
-    body.set_visible_child_name("0");
-
-    // Updating button callback does not work, you should recreate the buttons
-
     //retrieve_volumes();
 
     //window.fullscreen();
@@ -62,7 +140,7 @@ fn setup_gui(app: &Application) {
 fn setup_clock(clock: &gtk::Label) {
     let c = clock.clone();
     let time = Local::now();
-    c.set_text(&format!("{}:{}", time.hour(), time.minute()));
+    c.set_text(&format!("{:02}:{:02}", time.hour(), time.minute()));
     glib::timeout_add_seconds_local(60, move || {
         let time = Local::now();
         c.set_text(&format!("{:02}:{:02}", time.hour(), time.minute()));
